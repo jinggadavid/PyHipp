a9bb7d5825d343739217da953e09893c0548c6e4
AWS ParallelCluster user <ec2-user@ip-10-0-7-147.ap-southeast-1.compute.internal>
2021-11-09 08:54:33 +0000

    dsads

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -6,11 +6,11 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1      # number of processors per task
-#SBATCH -J "rplhighpass"   # job name
+#SBATCH -J "rplhighpass-sort"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rplhighpass-slurm.%N.%j.out # STDOUT
-#SBATCH -e rplhighpass-slurm.%N.%j.err # STDERR
+#SBATCH -o rplhighpass-sort-slurm.%N.%j.out # STDOUT
+#SBATCH -e rplhighpass-sort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 /data/miniconda3/bin/conda init
