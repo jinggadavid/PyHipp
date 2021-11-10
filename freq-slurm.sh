c8a0da1be1135093a7ea4ae043555268a40429a7
AWS ParallelCluster user <ec2-user@ip-10-0-14-168.ap-southeast-1.compute.internal>
2021-11-10 15:28:42 +0000

    sdasdasd

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -15,6 +15,7 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 import time; \
+pyh.RPLLFP(saveLevel=1); \
 pyh.FreqSpectrum(saveLevel=1); \
 pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
 print(time.localtime());"
